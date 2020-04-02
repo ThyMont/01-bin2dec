fdb6c6508c7095b93feed7065a030f5b8f88aa75
Thyago Monteiro <tmacademico@gmail.com>
2020-04-02 16:23:06 -0300

    Update Calculadora
    Ajuste na conversão de Binário para Decimal e adição da função de conversão de Decimal para Binário

--- bin2dec/src/control/Calculadora.java
+++ bin2dec/src/control/Calculadora.java
@@ -1,19 +1,58 @@
 package control;
 
 public class Calculadora {
-	public static int converterBin2Dec(String bin) {
-		int resposta = 0;
-		bin = bin.substring(bin.indexOf('1'));
-		int n = bin.length()-1;
-		
-		for (int i = 0; i<bin.length(); i++) {
+	private int resposta = 0;
+	private String respostaBin = "";
+
+	public boolean converterBin2Dec(String bin) {
+
+		for (int i = 0; i < bin.length(); i++) {
+			if (bin.charAt(i) != '0' && bin.charAt(i) != '1') {
+				return false;
+			}
+		}
+		try {
+			bin = bin.substring(bin.indexOf('1')).trim();
+		} catch (Exception e) {
+
+		}
+		int n = bin.length() - 1;
+
+		for (int i = 0; i < bin.length(); i++) {
 			if (bin.charAt(i) == '1') {
 				resposta += Math.pow(2, n);
 			}
 			n--;
 		}
-		
-		return resposta;
-		
+
+		return true;
+
 	}
+
+	public int getResposta() {
+		return resposta;
+	}
+
+	public boolean converterDec2Bin(String dec) {
+		int x = 0;
+		int resto = 0;
+		try {
+			x = Integer.parseInt(dec);
+		} catch (Exception e) {
+			return false;
+		}
+
+		while (x >= 1) {
+			resto = x % 2;
+			respostaBin = resto + respostaBin;
+			x = x / 2;
+		}
+
+		return true;
+	}
+
+	public String getRespostaBin() {
+		return respostaBin;
+	}
+
 }
